b75b81e9cb8b5540045cfc97d6f78ba57f74bd51
will <wylliam.bastiani@gmail.com>
2023-05-30 15:06:48 -0300

    visual selection color

--- colors/melange.lua
+++ colors/melange.lua
@@ -66,7 +66,7 @@ local highlight_groups = {
   Substitute = { bg = d.red, bold = bold },
   -- QuickFixLine = { },
   -- IncSearch = { },
-  Visual = { bg = a.sel },
+  Visual = { bg = a.com },
   -- VisualNOS = { },
 
   Conceal = { fg = a.com },
