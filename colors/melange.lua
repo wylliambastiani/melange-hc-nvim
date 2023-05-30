0008e2ccc455756599f83861156521eefc89d7e0
will <wylliam.bastiani@gmail.com>
2023-05-30 15:15:08 -0300

    .

--- colors/melange.lua
+++ colors/melange.lua
@@ -66,7 +66,7 @@ local highlight_groups = {
   Substitute = { bg = d.red, bold = bold },
   -- QuickFixLine = { },
   -- IncSearch = { },
-  Visual = { bg = b.red },
+  Visual = { bg = b.magenta },
   -- VisualNOS = { },
 
   Conceal = { fg = a.com },
